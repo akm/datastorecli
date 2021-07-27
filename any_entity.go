13680eadaad1af7ea8f66e4171e8befd5f2d486a
akm <akm2000@gmail.com>
2021-07-28 00:11:43 +0900

    :sparkles: 任意のエンティティを扱うAnyEntityを追加

---
+++ any_entity.go
@@ -0,0 +1,23 @@
+package datastorecli
+
+import "cloud.google.com/go/datastore"
+
+type AnyEntity map[string]interface{}
+
+func (x AnyEntity) Load(props []datastore.Property) error {
+	for _, prop := range props {
+		x[prop.Name] = prop.Value
+	}
+	return nil
+}
+
+func (x AnyEntity) Save() ([]datastore.Property, error) {
+	var props []datastore.Property
+	for k, v := range x {
+		props = append(props, datastore.Property{
+			Name:  k,
+			Value: v,
+		})
+	}
+	return props, nil
+}
