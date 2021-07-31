0503bdebb3d16c95c5f64d7545ac48c5e4fd5b88
akm <akm2000@gmail.com>
2021-07-31 11:15:03 +0900

    :recycle: key_encode.goを抽出

---
+++ cmd/datastorecli/key_encode.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"cloud.google.com/go/datastore"
+	"github.com/akm/datastorecli"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+func keyEncodeCommand() *cobra.Command {
+	var id int64
+	var name string
+	var encodedParent string
+	r := &cobra.Command{
+		Use:  "encode KIND",
+		Args: validateFirstArgAsKind,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			kind := args[0]
+
+			parentKey, err := datastorecli.DecodeKey(encodedParent)
+			if err != nil {
+				return err
+			}
+
+			var key *datastore.Key
+			if id != 0 {
+				key = datastore.IDKey(kind, id, parentKey)
+			} else if name != "" {
+				key = datastore.NameKey(kind, name, parentKey)
+			} else {
+				return errors.Errorf("key encode requires id or name")
+			}
+
+			fmt.Fprint(os.Stdout, key.Encode())
+			return nil
+		},
+	}
+	r.Flags().Int64Var(&id, "id", int64(0), "id")
+	r.Flags().StringVar(&name, "name", "", "name")
+	r.Flags().StringVar(&encodedParent, "encoded-parent", "", "Encoded parent key")
+
+	return r
+}
--- cmd/datastorecli/main.go
+++ cmd/datastorecli/main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strings"
 
-	"cloud.google.com/go/datastore"
 	"github.com/akm/datastorecli"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -25,40 +24,7 @@ func main() {
 		keyCommand := &cobra.Command{
 			Use: "key",
 		}
-		keyCommand.AddCommand((func() *cobra.Command {
-			var id int64
-			var name string
-			var encodedParent string
-			r := &cobra.Command{
-				Use:  "encode KIND",
-				Args: validateFirstArgAsKind,
-				RunE: func(cmd *cobra.Command, args []string) error {
-					kind := args[0]
-
-					parentKey, err := datastorecli.DecodeKey(encodedParent)
-					if err != nil {
-						return err
-					}
-
-					var key *datastore.Key
-					if id != 0 {
-						key = datastore.IDKey(kind, id, parentKey)
-					} else if name != "" {
-						key = datastore.NameKey(kind, name, parentKey)
-					} else {
-						return errors.Errorf("key encode requires id or name")
-					}
-
-					fmt.Fprint(os.Stdout, key.Encode())
-					return nil
-				},
-			}
-			r.Flags().Int64Var(&id, "id", int64(0), "id")
-			r.Flags().StringVar(&name, "name", "", "name")
-			r.Flags().StringVar(&encodedParent, "encoded-parent", "", "Encoded parent key")
-
-			return r
-		})())
+		keyCommand.AddCommand(keyEncodeCommand())
 
 		keyCommand.AddCommand((func() *cobra.Command {
 			validateArgs := func(cmd *cobra.Command, args []string) error {
