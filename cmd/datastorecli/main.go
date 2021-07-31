8a41b5fbc0a4aaaa0085eb4b1f961811e3374baf
akm <akm2000@gmail.com>
2021-07-31 11:26:25 +0900

    :recycle: format.goを抽出

--- cmd/datastorecli/main.go
+++ cmd/datastorecli/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/akm/datastorecli"
 	"github.com/spf13/cobra"
@@ -34,29 +32,4 @@ func main() {
 
 func newClient(projectID, namespace string) (*datastorecli.Client, error) {
 	return datastorecli.NewClient(projectID, namespace), nil
-}
-
-func formatData(d interface{}) error {
-	b, err := json.Marshal(d)
-	if err != nil {
-		return err
-	}
-	if _, err := os.Stdout.Write(b); err != nil {
-		return err
-	}
-	return nil
-}
-
-func formatArray(d *[]interface{}) error {
-	for _, i := range *d {
-		if err := formatData(i); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func formatStrings(d *[]string) error {
-	fmt.Fprintf(os.Stdout, "%s\n", strings.Join(*d, "\n"))
-	return nil
 }
