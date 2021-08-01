0716a649d6c2b0cefea62dd3e1c5fefa9aa8055b
akm <akm2000@gmail.com>
2021-08-01 11:14:11 +0900

    :recycle: AddCommandの使い方を修正

--- cmd/datastorecli/main.go
+++ cmd/datastorecli/main.go
@@ -13,18 +13,20 @@ func main() {
 		Use: "datastorecli",
 	}
 
-	rootCmd.AddCommand(cli.ConnectableCommandFunc(cli.QueryCommand)())
-	rootCmd.AddCommand(cli.ConnectableCommandFunc(cli.GetCommand)())
-	rootCmd.AddCommand(cli.ConnectableCommandFunc(cli.PutCommand)())
-	rootCmd.AddCommand(cli.ConnectableCommandFunc(cli.DeleteCommand)())
-	rootCmd.AddCommand((func() *cobra.Command {
-		keyCommand := &cobra.Command{
-			Use: "key",
-		}
-		keyCommand.AddCommand(cli.WithNamespace(cli.KeyEncodeCommand)())
-		keyCommand.AddCommand(cli.KeyDecodeCommand())
-		return keyCommand
-	})())
+	rootCmd.AddCommand(
+		cli.ConnectableCommandFunc(cli.QueryCommand)(),
+		cli.ConnectableCommandFunc(cli.GetCommand)(),
+		cli.ConnectableCommandFunc(cli.PutCommand)(),
+		cli.ConnectableCommandFunc(cli.DeleteCommand)(),
+		(func() *cobra.Command {
+			keyCommand := &cobra.Command{Use: "key"}
+			keyCommand.AddCommand(
+				cli.WithNamespace(cli.KeyEncodeCommand)(),
+				cli.KeyDecodeCommand(),
+			)
+			return keyCommand
+		})(),
+	)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
